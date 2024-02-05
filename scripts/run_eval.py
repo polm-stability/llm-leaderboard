9fced603b0d77418f553fd45cdc21937949ca49d
Paul McCann <paul.mccann@stability.ai>
2024-02-05 07:25:07 +0000

    refactor eval script

--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -2,6 +2,7 @@ import wandb
 from wandb.sdk.wandb_run import Run
 import os
 import sys
+import argparse
 from omegaconf import DictConfig, OmegaConf
 import pandas as pd
 sys.path.append('llm-jp-eval/src') 
@@ -11,9 +12,15 @@ from mtbench_eval import mtbench_evaluate
 from config_singleton import WandbConfigSingleton
 from cleanup import cleanup_gpu
 
-def main():
-    conf_file = sys.argv[1]
 
+def parse_args():
+    parser = argparse.ArgumentParser(prog="nejumi eval", description="Run nejumi evaluation")
+    parser.add_argument("config")
+
+    args = parser.parse_args()
+    return args
+
+def load_config(conf_file):
     # Configuration loading
     if os.path.exists(conf_file):
         cfg = OmegaConf.load(conf_file)
@@ -28,7 +35,9 @@ def main():
                 'run_name': 'default_run_name'
             }
         }
+    return cfg_dict
 
+def wandb_setup(cfg_dict, conf_file):
     # W&B setup and artifact handling
     wandb.login()
     run = wandb.init(
@@ -48,7 +57,7 @@ def main():
         if os.path.exists(conf_file):
             artifact_config_path = conf_file
         else:
-            # If "configs/config.yaml" does not exist, write the contents of run.config as a YAML configuration string
+            # If the config file does not exist, write the contents of run.config as a YAML configuration string
             instance = WandbConfigSingleton.get_instance()
             assert isinstance(instance.config, DictConfig), "instance.config must be a DictConfig"
             with open(conf_file, 'w') as f:
@@ -59,15 +68,17 @@ def main():
         artifact.add_file(artifact_config_path)
         run.log_artifact(artifact)
 
-    # Evaluation phase
-    # 1. llm-jp-eval evaluation
+    return run, cfg
+
+def run_llm_jp():
     evaluate()
     cleanup_gpu()
 
-    # 2. mt-bench evaluation
+def run_mt_bench():
     mtbench_evaluate()
     cleanup_gpu()
 
+def finish(run, cfg):
     # Logging results to W&B
     if cfg.wandb.log and run is not None:
         instance = WandbConfigSingleton.get_instance()
@@ -75,6 +86,16 @@ def main():
             "leaderboard_table": instance.table
         })
         run.finish()
+
+def main():
+    args = parse_args()
+    cfg_dict = load_config(args.config)
+    run, cfg = wandb_setup(cfg_dict, args.config)
+
+    run_llm_jp()
+    run_mt_bench()
+
+    finish(run, cfg)
 
 if __name__ == "__main__":
     main()
